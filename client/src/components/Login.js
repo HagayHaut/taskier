6777f8a623d516868ec86eefa343dd9d0614a7cb
Hagay Haut <hagayhaut@gmail.com>
2022-08-15 15:49:55 -0400

    add login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <>
+      <form>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter your username..."
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password..."
+        />
+        <input type="submit" />
+      </form>
+    </>
+  );
 };
 
 export default Login;
